553eb4ff4387cd7ebfbd3958ec6f87b9d0f701c2
namnd <namnguyennd7428@gmail.com>
2023-09-23 00:01:49 +0700

    heap, priority queue, max heap, min heap

--- src/com/dsa/Main.java
+++ src/com/dsa/Main.java
@@ -3,27 +3,42 @@ package com.dsa;
 import com.dsa.doublylinkedlist.DoublyLinkedList;
 import com.dsa.graph.Graph;
 import com.dsa.hash.HashTable;
+import com.dsa.heap.Heap;
 import com.dsa.linkedlist.LinkedList;
 import com.dsa.queue.Queue;
 import com.dsa.stack.Stack;
 import com.dsa.tree.BinarySearchTree;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class Main {
 
     public static void main(String[] args) {
-        Graph graph = new Graph();
-        graph.addVertex("A");
-        graph.addVertex("B");
-        graph.addVertex("C");
+        // Heap heap = new Heap();
+        // heap.insert(99);
+        // heap.insert(72);
+        // heap.insert(100);
+        // heap.remove();
+        // System.out.println(heap.getHeap());
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(99);
+        priorityQueue.add(72);
+        priorityQueue.add(100);
+        priorityQueue.add(5);
+        priorityQueue.add(15);
 
-        graph.addEdge("A", "B");
-        graph.addEdge("A", "C");
-        graph.addEdge("C", "B");
+        System.out.println(priorityQueue);
+        priorityQueue.remove();
+        System.out.println(priorityQueue);
+        Comparator<Integer> maxHeapComparator = Comparator.reverseOrder();
+        PriorityQueue<Integer> maxPriorityQueue = new PriorityQueue<>(maxHeapComparator);
+        maxPriorityQueue.add(99);
+        maxPriorityQueue.add(72);
+        maxPriorityQueue.add(100);
+        maxPriorityQueue.add(5);
+        maxPriorityQueue.add(15);
 
-        graph.removeVertex("B");
-
-        graph.printGraph();
-
-
+        System.out.println(maxPriorityQueue);
     }
 }
