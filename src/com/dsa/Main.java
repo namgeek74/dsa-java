5ce79a0ea9a3c7ebe1c1b26f58665fffd3cef410
namnd <namnguyennd7428@gmail.com>
2023-09-22 15:10:15 +0700

    hash table

--- src/com/dsa/Main.java
+++ src/com/dsa/Main.java
@@ -1,6 +1,7 @@
 package com.dsa;
 
 import com.dsa.doublylinkedlist.DoublyLinkedList;
+import com.dsa.hash.HashTable;
 import com.dsa.linkedlist.LinkedList;
 import com.dsa.queue.Queue;
 import com.dsa.stack.Stack;
@@ -9,15 +10,19 @@ import com.dsa.tree.BinarySearchTree;
 public class Main {
 
     public static void main(String[] args) {
-        BinarySearchTree binarySearchTree = new BinarySearchTree();
-        binarySearchTree.insert(5);
-        binarySearchTree.insert(7);
-        binarySearchTree.insert(12);
-        binarySearchTree.insert(3);
-        System.out.println(binarySearchTree.root.right.right.value);
-        System.out.println(binarySearchTree.contains(12));
-        System.out.println(binarySearchTree.contains(13));
-        System.out.println(binarySearchTree.contains(3));
+        HashTable hashTable = new HashTable();
+        hashTable.set("nails", 100);
+        hashTable.set("tile", 50);
+        hashTable.set("lumber", 80);
+        hashTable.set("bolts", 200);
+        hashTable.set("screws", 140);
+
+        hashTable.printTable();
+        System.out.println(hashTable.get("bolts"));
+        System.out.println(hashTable.get("boltsf"));
+        System.out.println(hashTable.get("lumber"));
+        System.out.println(hashTable.keys());
+
 
     }
 }
