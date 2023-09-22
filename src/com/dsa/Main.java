da5b3ae21ee84c4dc71fca1863769ff59c0dd1d0
namnd <namnguyennd7428@gmail.com>
2023-09-22 16:25:03 +0700

    graph with adjacency list

--- src/com/dsa/Main.java
+++ src/com/dsa/Main.java
@@ -1,6 +1,7 @@
 package com.dsa;
 
 import com.dsa.doublylinkedlist.DoublyLinkedList;
+import com.dsa.graph.Graph;
 import com.dsa.hash.HashTable;
 import com.dsa.linkedlist.LinkedList;
 import com.dsa.queue.Queue;
@@ -10,18 +11,18 @@ import com.dsa.tree.BinarySearchTree;
 public class Main {
 
     public static void main(String[] args) {
-        HashTable hashTable = new HashTable();
-        hashTable.set("nails", 100);
-        hashTable.set("tile", 50);
-        hashTable.set("lumber", 80);
-        hashTable.set("bolts", 200);
-        hashTable.set("screws", 140);
+        Graph graph = new Graph();
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
 
-        hashTable.printTable();
-        System.out.println(hashTable.get("bolts"));
-        System.out.println(hashTable.get("boltsf"));
-        System.out.println(hashTable.get("lumber"));
-        System.out.println(hashTable.keys());
+        graph.addEdge("A", "B");
+        graph.addEdge("A", "C");
+        graph.addEdge("C", "B");
+
+        graph.removeVertex("B");
+
+        graph.printGraph();
 
 
     }
