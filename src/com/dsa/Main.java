7273b1e1e2dac15573872faa0bfb104c9cc8ef1e
namnd <namnguyennd7428@gmail.com>
2023-09-22 12:07:45 +0700

    binary search tree

--- src/com/dsa/Main.java
+++ src/com/dsa/Main.java
@@ -4,15 +4,20 @@ import com.dsa.doublylinkedlist.DoublyLinkedList;
 import com.dsa.linkedlist.LinkedList;
 import com.dsa.queue.Queue;
 import com.dsa.stack.Stack;
+import com.dsa.tree.BinarySearchTree;
 
 public class Main {
 
     public static void main(String[] args) {
-        Queue queue = new Queue(1);
-        queue.enqueue(2);
-        queue.enqueue(3);
-        queue.dequeue();
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.insert(5);
+        binarySearchTree.insert(7);
+        binarySearchTree.insert(12);
+        binarySearchTree.insert(3);
+        System.out.println(binarySearchTree.root.right.right.value);
+        System.out.println(binarySearchTree.contains(12));
+        System.out.println(binarySearchTree.contains(13));
+        System.out.println(binarySearchTree.contains(3));
 
-        queue.printQueue();
     }
 }
---
+++ src/com/dsa/tree/BinarySearchTree.java
@@ -0,0 +1,55 @@
+package com.dsa.tree;
+
+public class BinarySearchTree {
+    public class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Node root;
+
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while (true) {
+            if (temp.value == value) return false;
+            if (value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
+
+    public boolean contains(int value) {
+        if (root == null) return false;
+        Node temp = root;
+        while (temp != null) {
+            if (value > temp.value) {
+                temp = temp.right;
+            } else if (value < temp.value) {
+                temp = temp.left;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
